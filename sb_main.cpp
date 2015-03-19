c357bfd31cb2649d7ff9d5785ff1672ac302da26
Drew Gonsalves <gonsalvd@gmail.com>
2015-03-19 17:32:15 -0400

    test working of reading in from din and checking true/false

--- sb_main.cpp
+++ sb_main.cpp
@@ -19,11 +19,32 @@
 using namespace std;
 
 //this is going to be RECURSIVE
-//
-void binarySearch (int NE, int NS, vector<int> all, int search[])
+//search CAN remain an array, does not need to be sorted in order to search through the venctor elementally
+void binarySearch (int NE, int left, int right, vector<int> all, int search_value)
 {
+    //eg 9/2 = 4 (due to 4.5 being casted as int to 4)
+    //left side will be from 0 to 4 and right side from 5 to 9
+    cout<<"got here"<<endl;
+    int middle = (right - left) / 2;
+    if (search_value == all.at(middle))
+    {
+        cout<<"Hit match of "<<search_value<<endl;
+        return;
+    }
+    //Equivalent to not finding it
+    if (left == right)
+    {
+        return;
+    }
+    if (search_value < all.at(middle))
+    {
+        return binarySearch(0, left, middle - left, all, search_value);
+    }
+    if (search_value > all.at(middle))
+    {
+        return binarySearch(0, middle + left, right, all, search_value);
+    }
     
-
 }
 
 void linearSearch (int NE, int NS, int all[], int search[])
@@ -52,10 +73,15 @@ void linearSearch (int NE, int NS, int all[], int search[])
 int main(int argc, char **argv)
 {
     
+    
     int NE;  // number of elements
     int NS;  // number of element to search
+    int exe_type;
     
-    if (argc != 3)
+    //changed for testing
+    //TWO types of inputs. Input for testing: ./search 10 5 1
+    //Input for taking in values: ./search 10 5 0
+    if (argc != 4)
     {
         printf("<usage>: parameters.\n");
         return 1;
@@ -65,88 +91,146 @@ int main(int argc, char **argv)
         NE = atoi(argv[1]);
         NS = atoi(argv[2]);
         //printf("Number of elements = %d\nNumber of elements to search = %d\n", NE, NS);
+        exe_type = atoi(argv[3]);
     }
     
     //Created holders for the elements
     int all_holder[NE];
     int search_holder[NS];
     
-    // Initializing seed for pseudo-random sequence
-    srand(time(NULL));
-    
-    printf("%d %d\n", NE, NS);
-    for (int i=0; i<NE; i++)
+    if(exe_type == 0)
     {
-        int number;
+        cin>>NE;
+        cin>>NS;
+        cout<<NE<<endl;
+        cout<<NS<<endl;
+        for (int a = 0;a < NE; a++)
+        {
+            cin>>all_holder[a];
+            cout<<"all holder:"<<all_holder[a]<<endl;
+        }
+        for (int b = 0; b < NS; b++)
+        {
+            cin>>search_holder[b];
+            cout<<"search holder:"<<search_holder[b]<<endl;
+        }
         
-        number = rand();
-        // coin toss
-        double r = ((double) rand() / (RAND_MAX));
+        clock_t start = 0;
+        clock_t finish = 0;
+        double elapsed_time = 0;
+        start = clock();
+        // Here goes the code you want to time
+        // ...
         
-        if (r < 0.05)           // Introducing neg. numbers with 5% probability
-            number = - number;
-        //printf("%d ", number);
+        //LINEAR SEARCH TIMING
+        linearSearch(NE,NS,all_holder,search_holder);
         
-        //added number to set
-        all_holder[i] = number;
+        cout<<"outside of binary"<<endl;
+        finish = clock();
+        elapsed_time = (double)(finish - start) / CLOCKS_PER_SEC;
+        cout<<"Value of N: "<<NE<<endl;
+        cout<<"Value of S: "<<NS<<endl;
+        cout<<"Time for LinearSearch: "<<elapsed_time<<endl;
         
-        cout<<all_holder[i]<<endl;
+        return 0;
         
-        //if (!(i % 100)) printf("\n");
-    }
-    printf("\n");
-    for (int i=0; i<NS; i++)
-    {
-        int number;
-        
-        number = rand();
-        // coin toss
-        double r = ((double) rand() / (RAND_MAX));
-        
-        if (r < 0.05)           // Introducing neg. numbers with 5% probability
-            number = - number;
-        //printf("%d ", number);
-        
-        //added
-        search_holder[i] = number;
-        cout<<search_holder[i]<<endl;
-
-        
-        //cout<<"here2"<<endl;
-        
-        //if (!(i % 100)) printf("\n");
     }
     
-    
-    int* holder[argc];
-    clock_t start = 0;
-    clock_t finish = 0;
-    double elapsed_time = 0;
-    
-    //Put all array values into a vector for easy sorting
-    vector<int> all_vector (all_holder, all_holder+NE);
-    sort(all_vector.begin(), all_vector.begin()+NE);
-    //cout<<"Array value at 0:"<<all_holder[0]<<endl;
-    
-    //Print out to confirm that everything was sorted
-    //Sortingis done OUTSIDE of the timing function for BOTH BINARY AND LINEAR SEARCH
-    for(int a=0; a<NE; a++)
+    if(exe_type == 1)
     {
-        cout<<"Vector value at "<<a<<": "<<all_vector.at(a)<<endl;
-
+        
+        
+        //Created holders for the elements
+        int all_holder[NE];
+        int search_holder[NS];
+        
+        // Initializing seed for pseudo-random sequence
+        srand(time(NULL));
+        
+        printf("%d %d\n", NE, NS);
+        for (int i=0; i<NE; i++)
+        {
+            int number;
+            
+            number = rand();
+            // coin toss
+            double r = ((double) rand() / (RAND_MAX));
+            
+            if (r < 0.05)           // Introducing neg. numbers with 5% probability
+                number = - number;
+            //printf("%d ", number);
+            
+            //added number to set
+            all_holder[i] = number;
+            
+            cout<<all_holder[i]<<endl;
+            
+            //if (!(i % 100)) printf("\n");
+        }
+        printf("\n");
+        for (int i=0; i<NS; i++)
+        {
+            int number;
+            
+            number = rand();
+            // coin toss
+            double r = ((double) rand() / (RAND_MAX));
+            
+            if (r < 0.05)           // Introducing neg. numbers with 5% probability
+                number = - number;
+            //printf("%d ", number);
+            
+            //added
+            search_holder[i] = number;
+            cout<<search_holder[i]<<endl;
+            
+            
+            //cout<<"here2"<<endl;
+            
+            //if (!(i % 100)) printf("\n");
+        }
+        
+        
+        int* holder[argc];
+        clock_t start = 0;
+        clock_t finish = 0;
+        double elapsed_time = 0;
+        
+        //Put all array values into a vector for easy sorting
+        vector<int> all_vector (all_holder, all_holder+NE);
+        sort(all_vector.begin(), all_vector.begin()+NE);
+        //cout<<"Array value at 0:"<<all_holder[0]<<endl;
+        
+        //Print out to confirm that everything was sorted
+        //Sortingis done OUTSIDE of the timing function for BOTH BINARY AND LINEAR SEARCH
+        for(int a=0; a<NE; a++)
+        {
+            cout<<"Vector value at "<<a<<": "<<all_vector.at(a)<<endl;
+            
+        }
+        
+        start = clock();
+        // Here goes the code you want to time
+        // ...
+        
+        //    //LINEAR SEARCH TIMING
+        //    linearSearch(NE,NS,all_holder,search_holder);
+        
+        //BINARY SEARCH TIMING
+        //    for (int a=0; a<NS; a++)
+        //    {
+        //        //Pasing all these variables is pretty poor programming. BinarySearch COULD just reference global variables, etc. Well maybe this isnt too bad and could be reused a little easier.
+        //        binarySearch(NE, 0, NE-1, all_vector, search_holder[a]);
+        //    }
+        cout<<"outside of binary"<<endl;
+        finish = clock();
+        elapsed_time = (double)(finish - start) / CLOCKS_PER_SEC;
+        cout<<"Value of N: "<<NE<<endl;
+        cout<<"Value of S: "<<NS<<endl;
+        cout<<"Time for LinearSearch: "<<elapsed_time<<endl;
     }
-    
-    start = clock();
-    // Here goes the code you want to time
-    // ...
-    linearSearch(NE,NS,all_holder,search_holder);
-    finish = clock();
-    elapsed_time = (double)(finish - start) / CLOCKS_PER_SEC;
-    cout<<"Value of N: "<<NE<<endl;
-    cout<<"Value of S: "<<NS<<endl;
-    cout<<"Time for LinearSearch: "<<elapsed_time<<endl;
     
     return 0;
-
+    
 }
 
